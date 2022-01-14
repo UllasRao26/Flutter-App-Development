037470f62f90feccfe6049afc8371e10050300aa
Ullas Rao Tuloji <ullasrao2005@gmail.com>
2022-01-14 21:48:52 +0530

    Created a basic Login Form

--- flutter_app_course/lib/pages/login_page.dart
+++ flutter_app_course/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_course/pages/home_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,14 +7,38 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.redAccent,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login.png"),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 35),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter your username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter your password", labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(child: Text("Login"), onPressed: () {}),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
